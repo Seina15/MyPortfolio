e1c1b7b67c25a18a139aa30770edccb4b9248d2a
Seina15 <inagawa.15.ai@gmail.com>
2025-05-27 16:11:28 +0900

    add about me

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css'
-import ResponsiveAppBar from "./ResponsiveAppBar.jsx"
+import ResponsiveAppBar from "./components/ResponsiveAppBar"
+import ScrollSection from "./components/ScrollSection/ScrollSection"
 
 
 function App() {
-  
   return (
     <>
-      <ResponsiveAppBar />
+      <ResponsiveAppBar/>
+      <div className='first-content'>
+        <h1 className="fade-in-welcome">Thank you for coming!</h1>
+      </div>
+      <div style={{ minHeight: '200vh' }}>
+        <ScrollSection />
+      </div>
     </>
   )
-  
 }
+
 
 export default App
---
+++ src/components/ScrollSection/ScrollSection.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './section.css';
+
+export default function ScrollSection() {
+  return (
+    <>
+      {/* このサイトについて */}
+      <section className="scroll-section" id="about-site">
+        <h1 className="title">~ About This Page ~</h1>
+        <p id="about-this-site">
+          ここは、私のポートフォリオサイトです。<br/>
+          大学生活の中で、得た経験や実績に関してまとめました。<br/>
+          私のことを少しでも知っていただけたら幸いです。
+        </p>
+      </section>
+
+      {/* 自己紹介 */}
+      <section className="scroll-section offset" id="about-me">
+        <h1 className="title" style={{ color: '#000000' }}>~ About Me ~</h1>
+        
+        <div className="profile">
+          <img src="/icon.png" alt="プロフィール写真" className="profile-photo" />
+          <p style={{ fontSize: '1.4rem' }}>
+            氏名：稲川 聖七（Seina Inagawa）<br/>
+            趣味：御朱印集め、観劇、プログラミング<br/>
+            関心：Webアプリケーション・ロボット
+          </p>
+        </div>
+        <hr/>
+        <h2>自己紹介</h2>
+          茨城県出身で、昔からロボットやSF映画が大好きでした。<br/>
+          コロナの影響で留学の計画が無くなって、たどり着いたのがプログラミング。<br/>
+          なにか新しいものを開発したくて、積極的にハッカソンやサークル活動に励んでいます。<br/>
+
+
+
+      </section>
+      
+      <section className="scroll-section" id="about-site">
+        <h1 className="title">~ About This Page ~</h1>
+        <p id="about-this-site">
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+
+        </p>
+      </section>
+      
+    </>
+  );
+}
